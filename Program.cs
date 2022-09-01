ff15cc06c24f0ffa1279c824f4ae1c1a6fbbf62f
Roman Permiakov <info@presstomsk.ru>
2022-09-01 23:44:20 +0700

    Add SplashWindow

--- Program.cs
+++ Program.cs
@@ -6,6 +6,7 @@ using AviaTickets.Processes;
 using AviaTickets.Processes.Abstractions;
 using AviaTickets.Scheduler;
 using AviaTickets.Scheduler.Abstractions;
+using AviaTickets.Splash.Logic;
 using AviaTickets.Validator;
 using AviaTickets.ViewModel;
 using AviaTickets.ViewModel.Absractions;
@@ -13,7 +14,8 @@ using FluentValidation;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Serilog;
-
+using System;
+using System.Windows;
 
 namespace AviaTickets
 {    public class Program
@@ -22,30 +24,42 @@ namespace AviaTickets
 
         public Program()
         {
-            var configuration = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+            try
+            {
+                using (var loadingVisualiser = new SplashWindow("loading..."))
+                {
+                    var configuration = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
 
-            var serilog = new LoggerConfiguration().ReadFrom.Configuration(configuration).CreateLogger();
+                    var serilog = new LoggerConfiguration().ReadFrom.Configuration(configuration).CreateLogger();
 
-            _serviceProvider = new ServiceCollection()                                    
-                                    .AddSingleton(configuration)
-                                    .AddLogging((config) => config.AddSerilog(serilog))
-                                    .AddSingleton<MainWindow>()                                   
-                                    .AddSingleton<IView, View>()                                    
-                                    .AddSingleton<CitiesConverter>()
-                                    .AddSingleton<TicketConverter>()
-                                    .AddSingleton<ICitiesListCreatingWorkflow, CitiesListCreatingWorkflow>()
-                                    .AddTransient<IInputDataValidationWorkflow, InputDataValidationWorkflow>()
-                                    .AddTransient<IAviaTicketsGetWorkflow, AviaTicketsGetWorkflow>() 
-                                    .AddTransient<IOpenTicketLinkWorkflow,OpenTicketLinkWorkflow>()
-                                    .AddTransient<ITicketsCreatedWorkflow,TicketsCreatedWorkflow>()
-                                    .AddTransient<IAddTicketsIntoView,AddTicketsIntoView>()
-                                    .AddSingleton<ISchedulerFactory, SchedulerFactory>()
-                                    .AddTransient<AbstractValidator<IView>, InputDataValidator>()
-                                    .AddTransient<ICities, Cities>()
-                                    .AddTransient<ITicket, Result>()                           
-                                    .BuildServiceProvider();
+                    _serviceProvider = new ServiceCollection()
+                                            .AddSingleton(configuration)
+                                            .AddLogging((config) => config.AddSerilog(serilog))
+                                            .AddSingleton<MainWindow>()
+                                            .AddSingleton<IView, View>()
+                                            .AddSingleton<CitiesConverter>()
+                                            .AddSingleton<TicketConverter>()
+                                            .AddSingleton<ICitiesListCreatingWorkflow, CitiesListCreatingWorkflow>()
+                                            .AddTransient<IInputDataValidationWorkflow, InputDataValidationWorkflow>()
+                                            .AddTransient<IAviaTicketsGetWorkflow, AviaTicketsGetWorkflow>()
+                                            .AddTransient<IOpenTicketLinkWorkflow, OpenTicketLinkWorkflow>()
+                                            .AddTransient<ITicketsCreatedWorkflow, TicketsCreatedWorkflow>()
+                                            .AddTransient<IAddTicketsIntoView, AddTicketsIntoView>()
+                                            .AddSingleton<ISchedulerFactory, SchedulerFactory>()
+                                            .AddTransient<AbstractValidator<IView>, InputDataValidator>()
+                                            .AddTransient<ICities, Cities>()
+                                            .AddTransient<ITicket, Result>()
+                                            .BuildServiceProvider();
 
-            new MainController(_serviceProvider);
+                    new MainController(_serviceProvider);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"{ex}");
+                Application.Current?.Shutdown();
+                return;
+            }
 
         }
     }
---
+++ Splash/SplashWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace AviaTickets.Splash
+{   
+    public partial class SplashWindow : Window
+    {
+        public SplashWindow()
+        {
+            InitializeComponent();
+        }        
+    }
+}
