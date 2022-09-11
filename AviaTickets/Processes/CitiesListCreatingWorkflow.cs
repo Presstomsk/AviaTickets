0c8c868ae5ccc42738da8247593ea8ef4aea2b91
Roman Permiakov <info@presstomsk.ru>
2022-09-11 19:01:13 +0700

    Рефакторинг

--- AviaTickets/Processes/CitiesListCreatingWorkflow.cs
+++ AviaTickets/Processes/CitiesListCreatingWorkflow.cs
@@ -1,8 +1,8 @@
 ﻿using AviaTickets.DB.Abstractions;
+using AviaTickets.Models;
 using AviaTickets.Models.Abstractions;
 using AviaTickets.Processes.Abstractions;
 using AviaTickets.Scheduler.Abstractions;
-using AviaTickets.Statuses;
 using AviaTickets.ViewModel.Absractions;
 using System.Linq;
 
@@ -28,17 +28,17 @@ namespace AviaTickets.Processes
                             
         }      
 
-        public Result Start()
+        public Statuses.Result Start()
         {    
             var result = _scheduler.Start();
-            return new Result { Success = result, Content = null };
+            return new Statuses.Result { Success = result, Content = null };
         }
 
         public void GetCities()
         {
             using (var context = _contextFactory.CreateContext())
             {
-                _viewModel.Cities = context.Cities.Select(x => x).ToList<ICities>();
+                _viewModel.Cities = context.Cities.Select(x => new Cities{ City = x.City, Code = x.Code}).ToList<ICities>();
             }
         }
     }
